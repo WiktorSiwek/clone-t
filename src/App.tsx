ae16aa8ee68c5c314a6b2c360af923fa915c29c3
Siwek <wrse@gft.com>
2019-01-21 11:35:23 +0100

    Create base

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
 import React, { Component } from 'react';
-import './App.scss';
-import logo from './logo.svg';
+import styled from 'styled-components';
+
+import Layout from './layout/Layout';
+
+const Container = styled.div`
+  height: 100vh;
+  background-color: #1DA1F2;
+`;
 
 class App extends Component {
   public render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Container>
+        <Layout />
+      </Container>
     );
   }
 }
---
+++ src/layout/Layout.tsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Layout extends Component {
+  public render() {
+    return (
+      <div>
+        Test
+      </div>
+    )
+  }
+}
